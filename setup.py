3acf5917024600fdc58fbecd47e875192a122cca
Lars Tingelstad <lars.tingelstad@ntnu.no>
2018-10-12 10:00:53 +0200

    Added client and server

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tpk4170',
+      version='1.0',
+      description='Python modules for the course TPK4128 Industrial Mechatronics at NTNU ',
+      author='Lars Tingelstad',
+      author_email='lars.tingelstad@ntnu.no',
+      url='https://www.ntnu.no/studier/emner/TPK4128',
+      packages=find_packages(),
+      )
