14b581bbc8a82ad3b00d4673774cf685e0618666
Lars Tingelstad <lars.tingelstad@ntnu.no>
2018-10-12 15:42:17 +0200

    Working server and client

--- tpk4128/client.py
+++ tpk4128/client.py
@@ -1,10 +1,46 @@
 # Echo client program
 import socket
+from PIL import Image
+import numpy as np
+import struct
+import matplotlib.pyplot as plt
+import cv2
+import pickle
 
-HOST = 'localhost'    # The remote host
+
+def image(rgba):
+    blue = np.bitwise_and(np.right_shift(rgba, 0), 0xff)
+    green = np.bitwise_and(np.right_shift(rgba, 8), 0xff)
+    red = np.bitwise_and(np.right_shift(rgba, 16), 0xff)
+    alpha = np.bitwise_and(np.right_shift(rgba, 24), 0xff)
+    im = np.stack((red, green, blue, alpha), axis=2) / 255.0
+    return im
+
+
+HOST = '10.42.0.1'    # The remote host
 PORT = 50007          # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+
+    k = 0
+    while True:
+        s.sendall(b'Hello, world')
+
+        img = b''
+        size = 0
+        while size < 1228800:
+
+            data = s.recv(4096)
+            img += data
+
+            if not data:
+                break
+
+            size += len(data)
+
+        img = np.frombuffer(img, np.int32).reshape(640, 480).T
+        img = image(img)
+
+        cv2.imshow('img', img)
+        if cv2.waitKey(30) == 27:  # Esc: 27
+            break
--- tpk4128/server.py
+++ tpk4128/server.py
@@ -1,16 +1,53 @@
-# Echo server program
 import socket
+import pygame
+import pygame.camera
+from pygame.locals import *
+import numpy as np
+import pickle
+
+DEVICE = '/dev/video0'
+SIZE = (640, 480)
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-while 1:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.sendall(data)
-conn.close()
+
+
+def camstream():
+    pygame.init()
+    pygame.camera.init()
+    camera = pygame.camera.Camera(DEVICE, SIZE, 'RGB')
+    camera.start()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+
+    while True:
+
+        data = conn.recv(1024)
+        print(data)
+        if not data:
+            break
+
+        buf = camera.get_image()
+        buf = pygame.surfarray.array2d(buf)
+        buf = buf.tostring()
+        sent = 0
+        while buf:
+            bytes_ = conn.send(buf)
+            sent += bytes_
+            buf = buf[bytes_:]
+            print(sent)
+
+    conn.close()
+
+    camera.stop()
+    pygame.quit()
+    return
+
+
+if __name__ == '__main__':
+    camstream()
