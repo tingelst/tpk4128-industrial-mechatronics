e8c0ac2f4176a11305c59598f92b191481425ffc
Lars Tingelstad <lars.tingelstad@ntnu.no>
2020-03-03 15:44:27 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tpk4128',
-      version='1.0',
+      version='spring-2020',
       description='Python modules for the course TPK4128 Industrial Mechatronics at NTNU',
       author='Lars Tingelstad',
       author_email='lars.tingelstad@ntnu.no',
