18f5a0e52c7284a9a597775057467a2702f07a45
Lars Tingelstad <lars.tingelstad@ntnu.no>
2020-03-03 15:22:20 +0100

    py2 -> py3

--- scripts/echo_client.py
+++ scripts/echo_client.py
@@ -2,10 +2,12 @@
 import socket
 
 HOST = 'localhost'    # The remote host
-PORT = 50007              # The same port as used by the server
+PORT = 50007          # The same port as used by the server
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 s.sendall('Hello, world')
 data = s.recv(1024)
 s.close()
-print 'Received', repr(data)
+
+print('Received', repr(data))
